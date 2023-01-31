1f5d9029fdfdb8d94ec06c46e13d5f78648a2c07
varshakumre8080 <123441994+varshakumre8080@users.noreply.github.com>
2023-01-31 19:42:04 +0530

    Add files via upload

---
+++ OrganizationService.java
@@ -0,0 +1,43 @@
+package com.OrganizationManagment.OrgManage.Service;
+
+import com.OrganizationManagment.OrgManage.Entities.Organization;
+import com.OrganizationManagment.OrgManage.Repository.OraganizationRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrganizationService {
+
+    @Autowired
+    private OraganizationRepo repoOr;
+
+
+    public Organization getOrg(int id) {
+
+        return this.repoOr.findById(id).get();
+    }
+
+    public Organization addOrg(Organization organization) {
+        return this.repoOr.save(organization);
+    }
+
+    public void deleteOrg(int id) {
+        Organization organization= repoOr.getById(id);
+        repoOr.delete(organization);
+    }
+
+    public Organization updateOrg(Organization organization) {
+
+        return this.repoOr.save(organization);
+    }
+
+    public List<Organization> getAll() {
+
+        List<Organization> list = repoOr.findAll();
+        return list;
+    }
+
+
+}
